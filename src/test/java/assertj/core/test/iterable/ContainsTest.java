9aa7889deff5a4f10dba99e2e6ace63ea2f39415
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2022-02-26 16:33:30 +0900

    containsを試した

--- src/test/java/assertj/core/test/iterable/ContainsTest.java
+++ src/test/java/assertj/core/test/iterable/ContainsTest.java
@@ -67,4 +67,60 @@ public class ContainsTest {
     AssertionError assertionError2 = expectAssertionError(() -> assertThat(duplicateActual).containsExactlyInAnyOrder("一郎", "二郎", "三郎", "五郎", "四郎", "一郎", "二郎"));
     then(assertionError2).hasMessageContaining("but the following elements were unexpected:");
   }
+
+  /**
+   * containsSequence は指定した配列通りの順番のリストが含まれているかをテストする
+   */
+  @Test
+  void containsSequenceTest() {
+    assertThat(actual).containsSequence("一郎", "二郎");
+    assertThat(actual).containsSequence("三郎", "四郎", "五郎");
+
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).containsSequence("二郎", "一郎"));
+    then(assertionError).hasMessageContaining("to contain sequence:");
+
+    // これもpassする
+    List<String> duplicateActual = List.of("1", "2", "3", "2", "4", "5");
+    assertThat(duplicateActual).containsSequence("2", "4");
+  }
+
+  /**
+   * containsSubsequence は指定した配列通りの順番通りのリストになっているかテストする
+   * containsSequenceでは指定した配列の間に値があるとエラーになるが、こちらはならない
+   */
+  @Test
+  void containsSubsequenceTest() {
+    assertThat(actual).containsSubsequence("一郎", "二郎");
+    assertThat(actual).containsSubsequence("三郎", "五郎");
+
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).containsSubsequence("二郎", "一郎"));
+    then(assertionError).hasMessageContaining("Expecting actual to contain the specified subsequence");
+  }
+
+  /**
+   * containsOnlyOnce は指定した値が1つだけしか存在しないことをテストする
+   */
+  @Test
+  void containsOnlyOnceTest() {
+    assertThat(actual).containsOnlyOnce("一郎");
+    assertThat(actual).containsOnlyOnce("三郎", "五郎");
+
+    List<String> duplicateActual = new ArrayList<>(actual);
+    duplicateActual.add("一郎");
+    assertThat(duplicateActual).containsOnlyOnce("二郎");
+
+    AssertionError assertionError = expectAssertionError(() -> assertThat(duplicateActual).containsOnlyOnce("一郎"));
+    then(assertionError).hasMessageContaining("but some elements were found more than once:");
+  }
+
+  /**
+   * containsAnyOf は指定した配列型の値のうち一つでも同じ値があるかテストする
+   */
+  @Test
+  void containsAnyOfTest() {
+    assertThat(actual).containsAnyOf("一郎", "二郎", "七郎");
+
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).containsAnyOf("六郎", "七郎"));
+    then(assertionError).hasMessageContaining("but none were found");
+  }
 }
